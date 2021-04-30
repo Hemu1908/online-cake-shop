26e2ea6bad4cf104a9317cdb9320fed48a60b582
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-04-30 11:16:45 +0530

    first commit

---
+++ src/test/java/com/practice/cake/OnlineCakeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCakeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
