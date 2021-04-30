48da7a571397986497780244618015b97075f5a2
Manthan Kansara <manthankansara7@gmail.com>
2021-04-30 17:52:58 +0530

    Added customer entity class

---
+++ src/main/java/com/practice/entity/Customer.java
@@ -0,0 +1,61 @@
+package com.practice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shop_customer")
+public class Customer {
+	@Id
+	@SequenceGenerator(name="customer_seq", initialValue=1001, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="customer_seq")
+	@Column(name = "customer_id")
+	int customerId;
+	String name;
+	String email;
+	String password;
+	String address;
+	String mobile;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+}
